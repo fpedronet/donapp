e9841169cfe38c1025e8cfd8dcd78955ade83333
Glenn Lozano <glozano@sistemas.com>
2022-02-16 15:23:01 -0500

    fecha

--- src/app/_model/tipodocumento.ts
+++ src/app/_model/tipodocumento.ts
@@ -2,4 +2,8 @@ export class TipoDocumento{
     nIdTipoDocu?: number;
     vDescripcion? : string;
     vAbrev? : string;
+    vTipo? : string;
+    nLongitud?: number;
+    nEsNumerico?: number;
+    nLongExacta?: number;
 }
