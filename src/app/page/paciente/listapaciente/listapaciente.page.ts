7f4817377df96b04d168ef3090f383f87ea5b46d
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-02-16 08:32:37 -0500

    merge

--- src/app/page/paciente/listapaciente/listapaciente.page.ts
+++ src/app/page/paciente/listapaciente/listapaciente.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listapaciente',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListapacientePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  inicio(){
+    this.router.navigate(['inicio']);
+  }
+
+  crearpaciente(){
+    this.router.navigate(['crearpaciente']);
+  }
 }
