dc712a68948358508c9abc69823d59869056188e
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-02-24 16:59:15 -0500

    merge

---
+++ android/app/src/main/java/com/developodo/donapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.developodo.donapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.developodo.donapp',
   appName: 'donapp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '18554699769-jbnc3dtjpncab79dtgcn4e1mngbk0fe0.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
