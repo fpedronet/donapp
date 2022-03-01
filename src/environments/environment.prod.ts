35ec7d771dcbe17623b8c6228ddfa60c0506c253
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-03-01 09:20:35 -0500

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  UrlApi:'http://localhost:48394/api',
-  UrlImage:'../../../../assets/',
+  UrlApi:'https://sitedev.poclab.pe/donappdev/api',  
+  UrlImage:'assets/',
 
   TOKEN_NAME: 'access_token',
   TOKEN_GOOGLE: 'access_google',
