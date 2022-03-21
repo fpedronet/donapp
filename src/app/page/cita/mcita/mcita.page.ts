d7b746d551c3f06a48f4e093e657bf3949e1b2d4
Glenn Lozano <glozano@sistemas.com>
2022-03-20 21:42:36 -0500

    filtro estados

--- src/app/page/cita/mcita/mcita.page.ts
+++ src/app/page/cita/mcita/mcita.page.ts
@@ -3,9 +3,11 @@ import { ModalController } from '@ionic/angular';
 
 import jsonTipoCita from 'src/assets/json/listacita.json';
 import jsonTipoDonacion from 'src/assets/json/listadonacion.json';
+import jsonEstado from 'src/assets/json/listaestado.json';
 
 import { TipoCita } from 'src/app/_model/tipocita';
 import { TipoDonacion } from 'src/app/_model/tipodonacion';
+import { Estado } from 'src/app/_model/estado';
 
 @Component({
   selector: 'app-mcita',
@@ -20,12 +22,15 @@ export class McitaPage implements OnInit {
 
   listaTipoCitas: TipoCita[] = [];
   listaTipoDonaciones: TipoDonacion[] = [];
+  listaEstados: Estado[] = [];
   @Input() selectTipoCita: number[] = [];
   @Input() selectTipoDonacion: number[] = [];
+  @Input() selectEstado: number[] = [];
 
   ngOnInit() {
     this.listartipocita();
     this.listartipodonacion();
+    this.listarestado();
   }
 
   
@@ -61,6 +66,34 @@ export class McitaPage implements OnInit {
     }
   }
 
+  listarestado(){
+    this.listaEstados = [];
+
+    for(var i in jsonEstado) {
+      let estado: Estado = {};
+
+      estado.nIdEstado = jsonEstado[i].nIdEstado;
+      estado.vDescripcion = jsonEstado[i].vDescripcion;
+      estado.vDetalle = jsonEstado[i].vDetalle;
+      estado.vMensaje = jsonEstado[i].vMensaje;
+
+      estado.icon = jsonEstado[i].icon;
+      estado.color = jsonEstado[i].color;
+      estado.visual = jsonEstado[i].visual;
+      estado.isChecked = this.selectEstado.includes(estado.nIdEstado);
+
+      this.listaEstados.push(estado);
+    }
+  }
+
+  get filtrarEstadosHistorial() {
+    return this.listaEstados.filter(e => e.nIdEstado >= 4);
+  }
+
+  get filtrarCancelado() {
+    return this.listaEstados.find(e => e.nIdEstado === 3);
+  }
+
   changeListCita(e: any){
     //debugger;
     /*var count = this.listaTipoCitas.filter(e => e.isChecked).length;
@@ -87,9 +120,18 @@ export class McitaPage implements OnInit {
       }
     });
 
+    //Tipos de donación seleccionados
+    this.selectEstado = [];
+    this.listaEstados.forEach(tipo => {
+      if(tipo.isChecked){
+        this.selectEstado.push(tipo.nIdEstado);
+      }
+    });
+
     this.modalCtrl.dismiss({
       arrayTipoCita:this.selectTipoCita,
-      arrayTipoDonacion:this.selectTipoDonacion
+      arrayTipoDonacion:this.selectTipoDonacion,
+      arrayEstado:this.selectEstado
     });
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,11 +11,11 @@ export const environment = {
     measurementId: "G-QWT46Q69ME"
   },
 
-  UrlApi:'https://sitedev.poclab.pe/donappdev/api',  
-  UrlImage:'assets/',
+  //UrlApi:'https://sitedev.poclab.pe/donappdev/api',  
+  //UrlImage:'assets/',
 
-    //  UrlApi:'http://localhost:48394/api',  
-    //  UrlImage:'assets/',
+      UrlApi:'http://localhost:48394/api',  
+      UrlImage:'assets/',
 
   TOKEN_NAME: 'access_token',
   TOKEN_GOOGLE: 'access_google',
